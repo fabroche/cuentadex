4da17f39ac57e2a7db2289bf06b1dbb665946cf8
fabroche <kraxusmmo@gmail.com>
2025-11-15 12:34:09 +0000

    feat: new Title added to the form, Submit Button label changed

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -11,30 +11,33 @@ function Form({setData}) {
     }
 
     return (
-        <form className="App-form" onSubmit={handleSubmit}>
-            <div>
+        <div className="App-form-container">
+            <h2>Actualizar Datos</h2>
+            <form className="App-form" onSubmit={handleSubmit}>
 
-                <label htmlFor="totalPokedex">Tamaño de la pokedex</label>
-                <input type="number" id="totalPokedex" name="totalPokedex" placeholder={230} required/>
-            </div>
-            <div>
-                <label htmlFor="totalCapturados">Total Capturados</label>
-                <input type="number" id="totalCapturados" name="totalCapturados" placeholder={167} required/>
-            </div>
-            <div>
-                <label htmlFor="cantidadDeDiaFaltante">Dias faltantes para completar la
-                    pokedex</label>
-                <input type="number" id="cantidadDeDiaFaltante" name="cantidadDeDiaFaltante" placeholder={7}
-                       required/>
-            </div>
-            <div>
+                <div>
+                    <label htmlFor="totalPokedex">Tamaño de la pokedex</label>
+                    <input type="number" id="totalPokedex" name="totalPokedex" placeholder={230} required/>
+                </div>
+                <div>
+                    <label htmlFor="totalCapturados">Total Capturados</label>
+                    <input type="number" id="totalCapturados" name="totalCapturados" placeholder={167} required/>
+                </div>
+                <div>
+                    <label htmlFor="cantidadDeDiaFaltante">Dias faltantes para completar la
+                        pokedex</label>
+                    <input type="number" id="cantidadDeDiaFaltante" name="cantidadDeDiaFaltante" placeholder={7}
+                           required/>
+                </div>
+                <div>
 
-                <label htmlFor="tiempoDisponibleDiario">Tiempo disponible diario (en minutos)</label>
-                <input type="number" id="tiempoDisponibleDiario" name="tiempoDisponibleDiario" placeholder={120}
-                       required/>
-            </div>
-            <button>Submit</button>
-        </form>
+                    <label htmlFor="tiempoDisponibleDiario">Tiempo disponible diario (en minutos)</label>
+                    <input type="number" id="tiempoDisponibleDiario" name="tiempoDisponibleDiario" placeholder={120}
+                           required/>
+                </div>
+                <button>Guardar Cambios</button>
+            </form>
+        </div>
     );
 }
 
