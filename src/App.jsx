7df8e5fcf74f1e71afa62aae6e5068fcf2ecc7de
fabroche <kraxusmmo@gmail.com>
2025-11-15 00:01:23 +0000

    adding some glow ups to the header

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,16 @@ import {useState} from "react";
 import banner from "./assets/images/banner.jpg"
 
 function App() {
-        // totalPokedex: 230,
-        // totalCapturados: 167,
-        // cantidadDeDiaFaltante: 7,
-        // tiempoDisponibleDiario: 120
+    // totalPokedex: 230,
+    // totalCapturados: 167,
+    // cantidadDeDiaFaltante: 7,
+    // tiempoDisponibleDiario: 120
 
     const [data, setData] = useState({
-        totalPokedex: 0,
-        totalCapturados: 0,
-        cantidadDeDiaFaltante: 0,
-        tiempoDisponibleDiario: 0
+        totalPokedex: 230,
+        totalCapturados: 167,
+        cantidadDeDiaFaltante: 7,
+        tiempoDisponibleDiario: 120
     });
 
     const tiempoNecesarioDeCapturaPorPokemonPorDia = calculateRemainingTime(data) || 0;
@@ -35,26 +35,26 @@ function App() {
     }
 
 
-  return (
-    <div className="App">
-        <header className="App-header">
-            <h1 className="App-title">Welcome to Cuentadex</h1>
-        </header>
-        <form className="App-form" onSubmit={handleSubmit}>
-            <label htmlFor="totalPokedex">Tamaño de la pokedex</label>
-            <input type="number" id="totalPokedex" name="totalPokedex" placeholder={230}/>
-            <label htmlFor="totalCapturados">Total de pokemons capturados</label>
-            <input type="number" id="totalCapturados" name="totalCapturados" placeholder={167}/>
-            <label htmlFor="cantidadDeDiaFaltante">Cantidad de dias faltantes para completar la pokedex</label>
-            <input type="number" id="cantidadDeDiaFaltante" name="cantidadDeDiaFaltante" placeholder={7}/>
-            <label htmlFor="tiempoDisponibleDiario">Tiempo disponible diario (en minutos)</label>
-            <input type="number" id="tiempoDisponibleDiario" name="tiempoDisponibleDiario" placeholder={120}/>
-            <button>Submit</button>
-        </form>
-
-        <h3>Necesitas capturar un pokemon cada <code> {tiempoNecesarioDeCapturaPorPokemonPorDia}</code> minutos</h3>
-    </div>
-  )
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1 className="App-title">Welcome to Cuentadex</h1>
+            </header>
+            <form className="App-form" onSubmit={handleSubmit}>
+                <label htmlFor="totalPokedex">Tamaño de la pokedex</label>
+                <input type="number" id="totalPokedex" name="totalPokedex" placeholder={230} required/>
+                <label htmlFor="totalCapturados">Total de pokemons capturados</label>
+                <input type="number" id="totalCapturados" name="totalCapturados" placeholder={167} required/>
+                <label htmlFor="cantidadDeDiaFaltante">Cantidad de dias faltantes para completar la pokedex</label>
+                <input type="number" id="cantidadDeDiaFaltante" name="cantidadDeDiaFaltante" placeholder={7} required/>
+                <label htmlFor="tiempoDisponibleDiario">Tiempo disponible diario (en minutos)</label>
+                <input type="number" id="tiempoDisponibleDiario" name="tiempoDisponibleDiario" placeholder={120}
+                       required/>
+                <button>Submit</button>
+            </form>
+            <h3>Necesitas capturar un pokemon cada <code> {tiempoNecesarioDeCapturaPorPokemonPorDia}</code> minutos</h3>
+        </div>
+    )
 }
 
 export default App
